f6ce621019af5bb3fc070ac268c28feec6009a5f
Rashed Aldawsari <raldawsari@SEDA.SA>
2020-11-09 19:00:11 +0300

    Fixing the socket io issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,7 +11,18 @@ export class HomePage {
   socket: any;
 
   constructor() {
-    this.socket = io.connect('https://test.peekabid.com/socket.io/');
+    console.log("initiating the connection");
+    try {
+      this.socket = io('https://test.peekabid.com', {
+        path: "/socket.io/"
+      });
+    } catch(e) { console.log(e); }
+
+    (window as any).socket = this.socket;
+
+    this.socket.on('connect', (data:any) => {
+      console.log('connected');
+    })
 
     this.socket.on('bid', (data: any) => {
       console.log(data);
